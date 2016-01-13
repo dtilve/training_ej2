aa5ef67f0391b311ada75eaf619c5a0bebc1b2a4
dtilve <tilve.dana@gmail.com>
2016-01-13 10:57:13 -0300

    Initial Commit

---
+++ ej2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ej2
+//
+//  Created by Dana Neyra on 1/13/16.
+//  Copyright © 2016 Dana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
